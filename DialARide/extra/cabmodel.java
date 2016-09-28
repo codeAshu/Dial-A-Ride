20554620fbf455f5f24f67202a2517b19f0b81c5
eshanraina <eshan.raina@gmail.com>
2016-09-28 16:53:20 -0700

    Delete cabmodel.java

--- DialARide/extra/cabmodel.java
+++
@@ -1,7 +0,0 @@
-CabID 			 	  	  A number given to each cab starting from zero.  
-location			  	  Location of the cab at it Timestamp
-timestamp			  	  TimeStamp of the cab		
-occupancy			  	  No of passanges in the cab
-Next Destination	  It stores the node where cab will go next
-Final Destination	  Towards which destination the cab is headed to. 
-timer 				  	  The time for which cab is sitting ideal
